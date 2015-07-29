56e423b03e68ca37e6413d0c02d1801fa6f8f27a
Allison Zentmayer <allison.zentmayer@excella.com>
2015-07-29 15:53:20 -0400

    Added StaticPages controller and views

--- nils_2_skills/config/routes.rb
+++ nils_2_skills/config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  get 'Home' => 'static_pages#Home'
+
+  get 'About' => 'static_pages#About'
+
+  get 'Help' => 'static_pages#Help'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'static_pages#Home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
