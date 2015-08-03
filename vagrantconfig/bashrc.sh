29d40dcbb68cee11d04ada9068ed45666b02b9e1
Brian Palladino <Brian.Palladino@excella.com>
2015-08-03 11:07:43 -0400

    Added accessdb alias for easier postgres cmd line access

--- vagrantconfig/bashrc.sh
+++ vagrantconfig/bashrc.sh
@@ -3,3 +3,4 @@ source /home/vagrant/.rvm/scripts/rvm
 
 # aliases
 alias runserver="rails server -b 0.0.0.0"  # need to use -b param to bind ip add
+alias accessdb="PGUSER=developer PGPASSWORD=developer psql -h localhost -p 5432 nils_2_skills_development"
