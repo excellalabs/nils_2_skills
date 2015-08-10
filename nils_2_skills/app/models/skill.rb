a0fe64cd51d6a553c70c5727f7f5c10270f5fcc1
Allison Zentmayer <allison.zentmayer@excella.com>
2015-08-10 10:27:20 -0400

    devplan testing

--- nils_2_skills/app/models/skill.rb
+++ nils_2_skills/app/models/skill.rb
@@ -1,7 +1,7 @@
 class Skill < ActiveRecord::Base
 	has_many :development_skills
 	has_many :skills_to_categories
-  	has_many :skill_categories, through :skills_to_categories
+  	has_many :skill_categories , through :skills_to_categories
 	validates :skill_name, presence: true
 	validates :category, presence: true
 end
