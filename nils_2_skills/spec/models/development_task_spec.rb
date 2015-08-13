7b6c28d6190a6e0313a5e0657056958a60574bde
Allison Zentmayer <allison.zentmayer@excella.com>
2015-08-13 12:11:32 -0400

    more rspec testing

--- nils_2_skills/spec/models/development_task_spec.rb
+++ nils_2_skills/spec/models/development_task_spec.rb
@@ -9,4 +9,8 @@ RSpec.describe DevelopmentTask do
   it "validates name length" do
     expect(DevelopmentTask.new(:task_name => ("a"*52))).to_not be_valid
   end
+
+  it "validates start date is before end date" do
+    expect(DevelopmentTask.new(:task_name => "Test", :start_date => Date.today, :completion_date => "2015-01-01")).to_not be_valid
+  end
 end
