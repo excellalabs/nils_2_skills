bfc5f52d8ec3988a005beebe7801e94964521ab6
Brian Palladino <Brian.Palladino@excella.com>
2015-08-14 09:52:51 -0400

    Added skill seed data

--- nils_2_skills/db/seeds.rb
+++ nils_2_skills/db/seeds.rb
@@ -12,6 +12,9 @@ require 'date'
 # Destroy all previous seeds
 User.destroy_all
 DevelopmentPlan.destroy_all
+Skill.destroy_all
+SkillCategory.destroy_all
+SkillLevel.destroy_all
 
 user_list = [
 	[ "Jon La Marr", "jon.lamarr@excella.com" ],
@@ -33,3 +36,43 @@ user_list.each do |name, email|
 								  	   description: "This is a development plan.")
 	end
 end
+
+# skill levels
+SkillLevel.create!(level: "Beginner", description: "Beginner with no experience" )
+SkillLevel.create!(level: "Junior", description: "Junior developer level" )
+SkillLevel.create!(level: "Mid", description: "Mid developer level" )
+SkillLevel.create!(level: "Senior", description: "Senior developer level" )
+
+# skill array
+skill_list = Array.new
+
+# Ruby
+ruby = SkillCategory.create!(category_name: "Ruby", description: "Ruby stack")
+skill_list << Skill.new(skill_name: "Ruby").skill_categories << ruby
+skill_list << Skill.new(skill_name: "Ruby on Rails").skill_categories << ruby
+
+# .NET
+net = SkillCategory.create!(category_name: ".NET", description: ".NET & Microsoft stack")
+skill_list << Skill.new(skill_name: "C#").skill_categories << net
+skill_list << Skill.new(skill_name: "ASP.NET").skill_categories << net
+
+# Java
+java = SkillCategory.create!(category_name: "Java", description: "Java stack")
+skill_list << Skills.new(skill_name: "Java").skill_categories << java
+
+# Python
+python = SkillCategory.create!(category_name: "Python", description: "Python stack")
+skill_list << Skill.new(skill_name: "Python").skill_categories << python
+skill_list << Skill.new(skill_name: "Django").skill_categories << python
+
+# Javascript
+js = SkillCategory.create!(category_name: "Javascript", description: "Javascript stack")
+skill_list << Skills.create!(skill_name: "Javascript").skill_categories << js
+skill_list << Skills.create!(skill_name: "Node.js").skill_categories << js
+
+# save skills to db
+skill_list.each	do |s|
+  s.save
+end
+
+
