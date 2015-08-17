d2224bfa38c3cb272343f3c02e2480b6aa4dc167
Kevin <kevin.ellis@excella.com>
2015-08-17 11:20:45 -0400

    Added drop down list for skills

--- nils_2_skills/app/controllers/development_skills_controller.rb
+++ nils_2_skills/app/controllers/development_skills_controller.rb
@@ -15,6 +15,7 @@ class DevelopmentSkillsController < ApplicationController
 
   def new
     @development_skill = DevelopmentSkill.new()
+    @skills = Skill.all()
     respond_with(@development_skill)
   end
 
