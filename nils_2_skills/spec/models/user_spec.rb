3d0fed322706670d3ea5bb1a467005eefb0c0284
unknown <jon.lamarr@excella.com>
2015-08-18 16:46:49 -0400

    Adding all user model tests for associations and validations

--- nils_2_skills/spec/models/user_spec.rb
+++ nils_2_skills/spec/models/user_spec.rb
@@ -1,39 +1,47 @@
 require "rails_helper"
 
 RSpec.describe User do
-	describe "Database Validation" do
-		it "must contain an email" do
-			user = User.new(:email => nil, :password => "password")
-			expect(user.save).to be false
+
+	describe "Model Associations" do
+		it "should have many development plans" do
+			should have_many(:development_plans)
 		end
 
-		it "must contain a valid email" do
-			user = User.new(:email => "invalidemailaddress", :password => "password")
-			expect(user.save).to be false
-		end
-
-		it "must contain a password" do
-			user = User.new(:email => "valid@email.com", :password => nil)
-			expect(user.save).to be false
-		end
-
-		it "must contain a valid password" do
-			user = User.new(:email => "valid@email.com", :password => "seven#s")
-			expect(user.save).to be false
-		end
-
-		it "must save with valid email and password" do
-			user = User.new(:email => "valid@email.com", :password => "ValidPassword22", 
-				:confirmed_at => Date.today) # Adding pre-confirmation for simplicity
-			expect(user.save).to be true
+		it "should have many development skills" do 
+			should have_many(:development_skills)
 		end
 	end
 
-	it "associates a development plan with a user" do
-		user = User.create!(username: "Allison", email: "allison.zentmayer@excella.com", password: "password",
-			created_at: Date.today, updated_at: Date.today, confirmed_at: Date.today)
-		plan = DevelopmentPlan.new( :plan_name => "Plan1", :description => "Desc")
-	    user.development_plans << plan
-	    expect(user.development_plans.count).to eq(1)
+	describe "Database Validation" do
+
+		it "should validate presence of email" do
+			validate_presence_of(:email)
+		end
+
+		it "should validate presence of password" do
+			validate_presence_of(:password)
+		end
+
+# Devise Testing - checks for validity on save
+		# Must match regex for emails
+		it "should contain a valid email" do
+			user = User.new(:email => "invalidemailaddress", 
+							:password => "password")
+			expect(user.save).to be false
+		end
+
+		# Password is invalid with under 8 characters
+		it "should contain a valid password" do
+			user = User.new(:email => "valid@email.com", 
+							:password => "seven#s")
+			expect(user.save).to be false
+		end
+
+		it "should save with valid email and password" do
+			user = User.new(:email => "valid@email.com", 
+							:password => "ValidPassword22", 
+							:confirmed_at => Date.today) # Adding pre-confirmation for simplicity
+			expect(user.save).to be true
+		end
 	end
 end
