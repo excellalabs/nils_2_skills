3896e011d1698764bd72213cca982792ac3ce9ef
Kevin <kevin.ellis@excella.com>
2015-08-19 12:08:31 -0400

    Rubocop fixes

--- nils_2_skills/test/controllers/development_tasks_controller_test.rb
+++ nils_2_skills/test/controllers/development_tasks_controller_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class DevelopmentTasksControllerTest < ActionController::TestCase
-
   # test "should get new" do
   #   get :new
   #   assert_response :success
--- nils_2_skills/test/controllers/skill_levels_controller_test.rb
+++ nils_2_skills/test/controllers/skill_levels_controller_test.rb
@@ -5,41 +5,41 @@ class SkillLevelsControllerTest < ActionController::TestCase
     @skill_level = skill_levels(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get :index
     assert_response :success
     assert_not_nil assigns(:skill_levels)
   end
 
-  test "should get new" do
+  test 'should get new' do
     get :new
     assert_response :success
   end
 
-  test "should create skill_level" do
+  test 'should create skill_level' do
     assert_difference('SkillLevel.count') do
-      post :create, skill_level: {  }
+      post :create, skill_level: {}
     end
 
     assert_redirected_to skill_level_path(assigns(:skill_level))
   end
 
-  test "should show skill_level" do
+  test 'should show skill_level' do
     get :show, id: @skill_level
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get :edit, id: @skill_level
     assert_response :success
   end
 
-  test "should update skill_level" do
-    patch :update, id: @skill_level, skill_level: {  }
+  test 'should update skill_level' do
+    patch :update, id: @skill_level, skill_level: {}
     assert_redirected_to skill_level_path(assigns(:skill_level))
   end
 
-  test "should destroy skill_level" do
+  test 'should destroy skill_level' do
     assert_difference('SkillLevel.count', -1) do
       delete :destroy, id: @skill_level
     end
