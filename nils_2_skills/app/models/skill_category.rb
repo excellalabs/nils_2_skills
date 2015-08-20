b42a70385b8948087d7f2312e9e4107aea54e03e
unknown <jon.lamarr@excella.com>
2015-08-20 11:23:00 -0400

    N2S 003: Added association/validation tests for skill_category model.

--- nils_2_skills/app/models/skill_category.rb
+++ nils_2_skills/app/models/skill_category.rb
@@ -1,5 +1,4 @@
 class SkillCategory < ActiveRecord::Base
-  validates :category_name, presence: true
-  validates :category_name, uniqueness: true
+  validates :category_name, presence: true, uniqueness: true
   has_and_belongs_to_many :skills
 end
