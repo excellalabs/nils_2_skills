255e3a7e46fd0c7e13a72c3d4faaa791beb52087
Brian Palladino <Brian.Palladino@excella.com>
2015-09-09 11:44:07 -0400

    N2S012, N2S014, fix association tags, update syntax to comply w rubocop standards

--- nils_2_skills/app/models/development_skill.rb
+++ nils_2_skills/app/models/development_skill.rb
@@ -1,8 +1,8 @@
 class DevelopmentSkill < ActiveRecord::Base
   belongs_to :skill
   belongs_to :user
-  belongs_to :current_skill_level, :class_name => "SkillLevel"
-  belongs_to :desired_skill_level, :class_name => "SkillLevel"
+  belongs_to :development_plan
+  belongs_to :current_skill_level, class_name: 'SkillLevel'
+  belongs_to :desired_skill_level, class_name: 'SkillLevel'
   has_many :development_tasks
-  has_and_belongs_to_many :development_plans
 end
